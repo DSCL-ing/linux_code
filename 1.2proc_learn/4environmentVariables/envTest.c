665879f57eb8497c14ea2e9e83e4f1181ae6ba77
chj <2639075023@qq.com>
2023-09-17 21:35:02 +0800

    update

--- 1.2proc_learn/4environmentVariables/envTest.c
+++ 1.2proc_learn/4environmentVariables/envTest.c
@@ -1,17 +1,29 @@
-
-
 #include<stdio.h>
 #include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+#define OWNER "chj"
 int main()
 {
-  extern char** environ;
-  for(int i = 0; environ[i];++i)
-  {
-    printf("envp[%d]->%s\n",i,environ[i]);
-  }
-
+    char* user = getenv("USER");   
+    if(!user) perror("getenv");
+    if(!strcmp(user,OWNER))  printf("合法\n");    
+    else printf("当前用户%s是非法用户\n",user);
     return 0;
 }
+
+
+//#include<stdio.h>
+//#include<unistd.h>
+//int main()
+//{
+//  extern char** environ;
+//  for(int i = 0; environ[i];++i)
+//  {
+//    printf("envp[%d]->%s\n",i,environ[i]);
+//  }
+//    return 0;
+//}
 
 
 // 1.显示传参方式接受main参数,使用main参数打印环境变量
