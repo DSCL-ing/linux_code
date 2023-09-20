d5215bf5de4bcb6557752ff44e13156030ecf5d1
chj <2639075023@qq.com>
2023-09-20 15:21:13 +0800

    update && create 1.2.7proc_wait

--- 1.2proc_learn/6returnStr_error/test.c
+++ 1.2proc_learn/6returnStr_error/test.c
@@ -1,9 +1,26 @@
 #include<stdio.h>
-#include<string.h>
+#include<string.h>//strerror
+#include<stdlib.h>//exit
+#include<unistd.h>//_exit
+int Sum()
+{
+  printf("start");
+  int n = 100;
+  int sum = 0;
+  for(int i = 0 ; i<n; ++i )
+  {
+      sum +=i;
+  }
+  _exit(11);
+  printf("end\n");
+  return 0;
+  
+}
+
 
 int main()
 {
-
+  int ret = Sum();
   for(int i = 0; i<200 ; ++i)
   {
     printf("%d : %s\n",i,strerror(i));
