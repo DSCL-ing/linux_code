c99ec0e8bdc85ac16aa2c781182a4ed07bb6bb36
chj <2639075023@qq.com>
2023-09-20 13:39:41 +0800

    create 1.2.6str_error

--- 1.2proc_learn/5commandLineParameter/clp.c
+++ 1.2proc_learn/5commandLineParameter/clp.c
@@ -9,9 +9,8 @@ void Usage(char* name)
 int main(int argc,char* argv[])
 {
     if(argc!=2) Usage(argv[0]);
-      if(!strcmp(argv[1],"-a")) printf("执行a功能\n");
+      if(!strcmp(argv[1],"-a")) printf("执行a功能\n"); 
       else if(!strcmp(argv[1],"-b")) printf("执行b功能\n");
-      else if(!strcmp(argv[1],"-c")) printf("执行c功能\n");
       else printf("其他功能\n");
     return 0;
 }
