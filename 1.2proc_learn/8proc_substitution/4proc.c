8a5d1ae1263a22d5e8dd443e88a1766234f8d893
chj <2639075023@qq.com>
2023-09-23 12:21:51 +0800

    update

--- 1.2proc_learn/8proc_substitution/4proc.c
+++ 1.2proc_learn/8proc_substitution/4proc.c
@@ -2,7 +2,8 @@
 #include<unistd.h>
 #include<sys/types.h> //getpid
 #include<sys/wait.h> //wait
-#include<stdlib.h>  //exit
+#include<stdlib.h>  //exit , putenv
+
 int main()
 {
     // 查看exec的返回值
@@ -11,19 +12,31 @@ int main()
     {
         printf("我是子进程: %d\n",getpid());
 //C++11要求字符串常量带const,不然会警告,警告是编译器的问题,C语言不受影响,无视即可
-         char * myargv[]={
-            "ls",
-            "-a",
-            "-l",
-            "-n",
-            NULL
-        };
+       //  char * myargv[]={
+       //     "ls",
+       //     "-a",
+       //     "-l",
+       //     "-n",
+       //     NULL
+       // };
        //1.execl//   int n = execl("/usr/bin/ls","ls","-a","-l",NULL);
        //2.execv//   int n = execv("/usr/bin/ls",myargv);
        //3.execlp//  int n = execlp("ls","ls","-a","-l","-n",NULL);
        //4.execvp//  int n = execvp("ls",myargv);
-        //int n = execle("ls","ls","-a","-l","-n",NULL,);
-        //printf("看到我说明execl执行失败了:返回值:%d\n",n);
+       // char *const myenv[] = {  //const是函数形参需要
+       //     "MYENV=我是传给C++程序的环境变量",
+       //     NULL
+       // };
+       
+        //传自定义的环境变量
+        //int n = execle("./test_CPP_proc/a.outProc_Cpp","a.outProc_Cpp",NULL,myenv);
+        extern char **environ;
+        //传系统的环境变量
+        
+        putenv("MYENV=我是传给C++程序的环境变量");
+        
+        int n = execle("./test_CPP_proc/a.outProc_Cpp","a.outProc_Cpp",NULL,environ); 
+        printf("看到我说明execl执行失败了:返回值:%d\n",n);
         exit(1);
     }
     sleep(1);
--- 1.2proc_learn/8proc_substitution/test_CPP_proc/otherproc.cc
+++ 1.2proc_learn/8proc_substitution/test_CPP_proc/otherproc.cc
@@ -11,9 +11,11 @@ int main()
 {
     for(int i = 0 ; i<5; ++i)
     {
+        cout<<"-----------------------------------------------------------"<<endl;
         cout<<"我是一个C++程序"<<" ";
-        cout<<"pid:"<< getpid() <<" ";
+        cout<<"pid:"<< getpid() <<endl;
         cout<<"MYENV:"<<(getenv("MYENV")==NULL?"NULL":getenv("MYENV"))<<endl;
+        cout<<"PATH:"<<(getenv("PATH")==NULL?"NULL":getenv("PATH"))<<endl;
         sleep(1) ;
     }
     return 0;
