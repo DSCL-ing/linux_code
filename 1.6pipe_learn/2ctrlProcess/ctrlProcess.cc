cb79d6bb6082d7ba8fbc9240cf0ef08ba322ac6b
chj <2639075023@qq.com>
2023-10-22 13:22:04 +0800

    匿名管道进程池

--- 1.6pipe_learn/2ctrlProcess/ctrlProcess.cc
+++ 1.6pipe_learn/2ctrlProcess/ctrlProcess.cc
@@ -5,6 +5,9 @@
 #include<vector>
 #include"Task.hpp"
 #include<cstdlib>
+#include<sys/types.h>
+#include<sys/wait.h>
+
 using namespace std;
 
 const int gnum = 5;//创建管道的数量  -- 代替宏的全局常量
@@ -52,7 +55,9 @@ void WaitCommand()
 }
 
 void createProcess(std::vector<endPoint> &end_points)
-{  for(int i = 0; i<gnum; ++i)
+{ 
+  std::vector<int> fds; //fd的复数形式
+  for(int i = 0; i<gnum; ++i)
   {
     //1.创建管道
     int pipefd[2] = {0};
@@ -66,6 +71,9 @@ void createProcess(std::vector<endPoint> &end_points)
 
     if(id == 0)//子进程
     {
+      for(const auto& fd:fds){
+        close(fd);//关闭继承自父进程的连接到其他子进程的写端
+      }
       close(pipefd[1]);
       
       //希望子进程从标准输入中读取,可做可不做,练习重定向使用
@@ -82,6 +90,9 @@ void createProcess(std::vector<endPoint> &end_points)
     
     //父进程:
     close(pipefd[0]);//关闭不要的fd
+
+    //记录父进程控制的管道写端
+    fds.push_back(pipefd[1]);
 
     //父进程只需要有进程id和对应的管道写端即可:
     end_points.push_back(endPoint(id,pipefd[1])); 
@@ -131,8 +142,44 @@ void ctrlProcess(std::vector<endPoint> *end_points)
 
 void waitProcess(std::vector<endPoint> &end_points)
 {
+  //1.要让使用管道的子进程退出,只需要让父进程关闭所有的write_fd就可以
+  //2.父进程回收子进程的僵尸状态
 
+
+//3.逆向写法:由管道进程复制逻辑,关闭最后一个,次低个就会正常,依次往上...
+//    可以说也是取巧方法 -- 对于不标准的进程池来说
+// for(int end = end_points.size()-1; end>=0;--end){
+//     close(end_points[end]._write_fd);
+//     std::cout<<"父进程让子进程"<<getpid()<<"退出"<<std::endl;
+//     sleep(2);
+//     waitpid(end_points[end]._child_id,nullptr,0);
+//     std::cout<<"父进程回收了所有子进程"<<std::endl;
+// }
+
+//2.正向写法:一个接一个关闭,如果不是标准的进程池,则可能会卡住:
+//  必须是关闭所有无用的fd的进程池才可以
+  for(const auto &ep:end_points) {
+    close(ep._write_fd);
+    std::cout<<"父进程让子进程"<<ep._child_id<<"退出"<<std::endl;
+    waitpid(ep._child_id,nullptr,0);
+    std::cout<<"父进程回收子进程"<<ep._child_id<<std::endl;
+  }
+
+
+//1.巧合写法:先全部关闭后再全部回收,这样也能实现关闭全部写端
+/* 
+  //1.要让使用管道的子进程退出,只需要让父进程关闭所有的write_fd就可以
+  //  严格来说应该是关闭一个子进程的全部写端,才能把这个子进程关闭
+  for(const auto &ep:end_points) close(ep._write_fd);
+  std::cout<<"父进程让所有的子进程退出"<<std::endl;
+  sleep(2);
+  //2.父进程回收子进程的僵尸状态
+  for(const auto &ep:end_points) waitpid(ep._child_id,nullptr,0);
+  std::cout<<"父进程回收了所有子进程"<<std::endl;
+*/
 }
+
+
 //构建一个多管道进程控制程序
 int main()
 {
