fff2f5238282211971ae5007f12979fb4f1ed423
chj <2639075023@qq.com>
2023-10-28 14:48:45 +0800

    signal

--- 18.signal/signal.cc
+++ 18.signal/signal.cc
@@ -1,3 +1,26 @@
 
+#include<iostream>
 #include<signal.h>
-SIGHUP
+#include<unistd.h>
+#include<sys/types.h>
+
+//sighandler_t == void(*)(int)
+void handler(int signu)
+{
+  std::cout<<"get a signal: " <<signu <<std::endl;
+}
+
+int main()
+{
+
+  signal(2,handler);
+
+  while(true)
+  {
+    std::cout<<"我是一个进程,我正在运行 ... ,pid: "<<getpid()<<std::endl;
+    sleep(1);
+  }
+
+
+  return 0 ;
+}
