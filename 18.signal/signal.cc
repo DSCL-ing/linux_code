e097fc05deb363dbb0c6776aeec477fa6c67135e
chj <2639075023@qq.com>
2023-10-29 17:56:43 +0800

    core dump practice

--- 18.signal/signal.cc
+++ 18.signal/signal.cc
@@ -7,7 +7,8 @@
 //sighandler_t == void(*)(int)
 void handler(int signu)
 {
-  std::cout<<"get a signal: " <<signu <<std::endl;
+  std::cout<<"get a signal: " <<signu <<"pid: "<<getpid()<<std::endl;
+  sleep(1);
 }
 
 int main()
@@ -16,11 +17,27 @@ int main()
   //signal(2,handler);
   //signal(3,handler);
 
-  while(true)
-  {
-    std::cout<<"我是一个进程,我正在运行 ... ,pid: "<<getpid()<<std::endl;
-    sleep(1);
-  }
+
+
+  // signal(8,handler);
+  // std::cout<<"除零错误"<<std::endl;
+  // std::cout<<"除零错误"<<std::endl;
+  // std::cout<<"除零错误"<<std::endl;
+  // int a =1/0; //除零错误
+
+    std::cout<<"野指针"<<std::endl;
+    std::cout<<"野指针"<<std::endl;
+    std::cout<<"野指针"<<std::endl;
+    std::cout<<"野指针"<<std::endl;
+  int *p = NULL; 
+  *p = 10; //野指针
+
+
+ // while(true)
+ // {
+ //   std::cout<<"我是一个进程,我正在运行 ... ,pid: "<<getpid()<<std::endl;
+ //   sleep(1);
+ // }
 
 
   return 0 ;
