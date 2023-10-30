88af8a44d84bab1caf7dd46963de2eeaada5d346
chj <2639075023@qq.com>
2023-10-30 15:29:23 +0800

    signal set

--- 18.signal/signal.cc
+++ 18.signal/signal.cc
@@ -11,24 +11,60 @@ void handler(int signu)
   sleep(1);
 }
 
+void showBlock(sigset_t* oset)
+{
+  int signo =1;
+  for(;signo<=31;++signo)
+  {
+    if(sigismember(oset,signo)==1) std::cout<<1;
+    else std::cout<<0;
+  }
+  std::cout<<"\n";
+}
+
 int main()
 {
+
+
+  sigset_t set,oset;
+  sigemptyset(&set);
+  sigemptyset(&oset);
+  sigaddset(&set,2);
+  sigprocmask(SIG_SETMASK,&set,&oset);
+
+  int cnt = 0;
+  while(true)
+  {
+    showBlock(&oset) ;
+    sleep(1);
+    if(cnt == 10)
+    {
+      std::cout<<"recover block\n";
+      sigprocmask(SIG_SETMASK,&oset,&set);
+      showBlock(&set);
+    }
+    cnt++;
+
+  }
+
+
 
   //signal(2,handler);
   //signal(3,handler);
   
-  signal(2,SIG_DFL);
+ // signal(2,SIG_DFL);
   
   
   //signal(2,SIG_IGN);
-  sleep(1);
+  //sleep(1);
   //std::cout<<"DFL"<<std::endl;
   //std::cout<<"IGN"<<std::endl;
   //raise(2);
-  while(true)
-  {
-    sleep(1);
-  }
+  
+  //while(true)
+  //{
+  //  sleep(1);
+  //}
   
 
   // signal(8,handler);
