bdc5f557ea4812a87eda4c471eec876cb421ab11
chj <2639075023@qq.com>
2023-11-05 13:41:08 +0800

    thread

--- 21.thread/3thread/thread.cc
+++ 21.thread/3thread/thread.cc
@@ -5,12 +5,14 @@
 #include <cstring>
 #include <string>
 
+
+
 std::string toHex(int num) 
 {
 	char buffer[64] ={0};
 	snprintf(buffer,64,"0x%x",num);
 	return buffer;
- }
+}
 
  void *threadRoutine(void *args)
  {
@@ -19,7 +21,7 @@ std::string toHex(int num)
    int cnt = 5;
    while(cnt)
    {
-     std::cout<<name<<" : "<<cnt--<<std::endl;
+     std::cout<<name<<" : "<<cnt--<<toHex(pthread_self())<<std::endl;
      sleep(1);
    }
    return nullptr;
@@ -29,9 +31,20 @@ std::string toHex(int num)
  {
      pthread_t tid;
      pthread_create(&tid, nullptr, threadRoutine, (void*)"thread 1");
-     sleep(10);                                                      
-     
+     //sleep(10);                                                      
+    
+     while(true)
+     {
+       std::cout<<" main thread: "<<toHex(pthread_self())<<" new thread id: " << toHex(tid)<<std::endl;
+       sleep(1);
+     }
+
      //pthread_detach(tid); //主线程分离
+     int n = pthread_join(tid,nullptr);
+     if(n!=0)
+     {
+       std::cout<<"error: "<<errno<<" : " <<strerror(errno)<<std::endl;
+     }
 
      std::cout << "all thread quit" << std::endl;
  
