d7c72359426dda198e57afbe5e593f237713bfae
chj <2639075023@qq.com>
2023-11-05 10:54:47 +0800

    thread

---
+++ 21.thread/2thread/thread.cc
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <pthread.h>
+#include <unistd.h>
+#include <cstring>
+#include <string>
+
+ void *threadRoutine(void *args)
+ {
+   pthread_detach(pthread_self()); // ----------------------复合函数实现自己分离自己
+   std::string name = static_cast<const char*>(args);
+   int cnt = 5;
+   while(cnt)
+   {
+     std::cout<<name<<" : "<<cnt--<<std::endl;
+     sleep(1);
+   }
+   return nullptr;
+ }
+ 
+ int main()
+ {
+     pthread_t tid;
+     pthread_create(&tid, nullptr, threadRoutine, (void*)"thread 1");
+     sleep(10);                                                      
+     
+     //pthread_detach(tid); //主线程分离
+
+     std::cout << "all thread quit" << std::endl;
+ 
+   return 0;
+ }
