513f13d1af93d3a607b4a2d290cbff8bd526330f
chj <2639075023@qq.com>
2023-11-07 18:12:15 +0800

    lock control

---
+++ 21.thread/7lock/thread.cc
@@ -0,0 +1,39 @@
+
+
+#include<iostream>
+#include<pthread.h>
+#include<unistd.h>  
+#include<cstring>
+#include<cerrno>
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void* routine(void*)
+{
+    std::cout<<"I am a new thread" <<std::endl;
+    pthread_mutex_lock(&mutex);
+    std::cout<<"I got a mutex!" <<std::endl;
+    pthread_mutex_lock(&mutex);
+    std::cout<<"I alive again" << std::endl;
+
+}
+
+int main()
+{
+    pthread_t tid;
+    int n = pthread_create(&tid,nullptr,routine,nullptr);
+    if(n!=0)
+    {
+        std::cout<<"phtread_create error"<<std::endl;
+        std::cout<<errno<<" : " <<strerror(errno)<<std::endl;
+    }
+
+    sleep(3);
+    std::cout<<"main thread run begin" <<std::endl;
+    pthread_mutex_unlock(&mutex);
+    std::cout<<"main thread unlock" <<std::endl;
+    sleep(3);
+    
+
+    return 0;
+}
